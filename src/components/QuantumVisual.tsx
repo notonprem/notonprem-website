83591baebd0c5116d4a98d9098efc39c5d322160
Foluso Ogunsakin <folusomaine@gmail.com>
2025-02-08 00:54:20 +0000

    update image1

--- src/components/QuantumVisual.tsx
+++ src/components/QuantumVisual.tsx
@@ -3,7 +3,7 @@ export const QuantumVisual = () => {
   return (
     <div className="w-full bg-[#1A1F2C]">
       <img
-        src="/lovable-uploads/5bc527ea-1fcb-4566-8d64-347e5926bf9f.png"
+        src="/lovable-uploads/dancing-dave-minion-510835.jpg"
         alt="Quantum Computing Visualization"
         className="w-full h-auto object-cover"
       />
